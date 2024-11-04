ae7c0c4901829d15b38e5b1cfebf87c6786b5d76
FreezeV5 <tarabulakrzysztof@gmail.com>
2024-11-04 14:43:08 +0100

    PR and push pipeline check

--- cw1/zad2a.py
+++ cw1/zad2a.py
@@ -2,4 +2,4 @@ def function_zad2a(names: []):
     for names in names:
         print(names)
 
-function_zad2a(["imie1","imie2","imie3","imie4","imie5"])
+function_zad2a(["imie1", "imie2", "imie3", "imie4", "imie5"])
