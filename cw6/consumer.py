cdf7a1b0d7ddb19cd85074dc14aab439d1b29048
FreezeV5 <tarabulakrzysztof@gmail.com>
2024-11-25 14:05:44 +0100

    tasks from 18.11.2024

--- cw6/consumer.py
+++ cw6/consumer.py
@@ -1,28 +1,23 @@
 import csv
 import time
 import os
-from datetime import datetime
 
 
 def consume_tasks(file_name='tasks.csv', lock_file='tasks.lock'):
     while True:
-        # Sprawdź, czy plik istnieje
         if not os.path.exists(file_name):
             print("No tasks file found. Waiting...")
             time.sleep(5)
             continue
 
-        # Odczytujemy zadania z pliku
         tasks = []
         pending_tasks = []
 
-        # Tworzymy blokadę pliku
         if os.path.exists(lock_file):
             print("File is locked by another process. Retrying...")
             time.sleep(2)
             continue
         try:
-            # Tworzymy blokadę
             open(lock_file, 'w').close()
 
             with open(file_name, 'r', newline='') as csvfile:
@@ -32,21 +27,17 @@ def consume_tasks(file_name='tasks.csv', lock_file='tasks.lock'):
                     if row["status"] == "pending":
                         pending_tasks.append(row)
         finally:
-            # Usuwamy blokadę
             if os.path.exists(lock_file):
                 os.remove(lock_file)
 
-        # Sprawdzamy, czy są zadania do wykonania
         if pending_tasks:
-            task_to_process = pending_tasks[0]  # Pobieramy pierwsze zadanie z kolejki
+            task_to_process = pending_tasks[0]
             print(f"Consuming task {task_to_process['id']}")
 
-            # Aktualizujemy status na "in_progress"
             for task in tasks:
                 if task["id"] == task_to_process["id"]:
                     task["status"] = "in_progress"
 
-            # Zapisujemy status "in_progress" z blokadą
             try:
                 open(lock_file, 'w').close()
                 with open(file_name, 'w', newline='') as csvfile:
@@ -57,15 +48,12 @@ def consume_tasks(file_name='tasks.csv', lock_file='tasks.lock'):
                 if os.path.exists(lock_file):
                     os.remove(lock_file)
 
-            # Symulujemy wykonywanie zadania
             time.sleep(30)
 
-            # Aktualizujemy status na "done"
             for task in tasks:
                 if task["id"] == task_to_process["id"]:
                     task["status"] = "done"
 
-            # Zapisujemy status "done" z blokadą
             try:
                 open(lock_file, 'w').close()
                 with open(file_name, 'w', newline='') as csvfile:
